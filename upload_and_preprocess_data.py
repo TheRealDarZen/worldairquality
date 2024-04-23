55dee82e3d8217b5874b594076631bf3b5c9c591
DarZen <nivnik004@gmail.com>
2024-04-23 10:43:59 +0200

    Added data downloading, temporarily disabled data preprocessing

--- upload_and_preprocess_data.py
+++ upload_and_preprocess_data.py
@@ -8,24 +8,25 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
 
 #Details
-file_name = "openaq.csv"
-dataset_path = "mexwell/world-air-quality"
+file_names = ["openaq.csv", "world_population.csv"]
+dataset_paths = ["mexwell/world-air-quality", "iamsouravbanerjee/world-population-dataset"]
 
 #Data dowlnoad using API
-if not os.path.isfile(file_name):
-    print(f"Started downloading {file_name} from kaggle.com...")
-    kaggle.api.dataset_download_files(dataset_path, path=".", unzip=True)
-    print("Download completed successfully.")
-else:
-    print(f"File {file_name} already exists.")
+for file_name, dataset_path in zip(file_names, dataset_paths):
+    if not os.path.isfile(file_name):
+        print(f"Started downloading {file_name} from kaggle.com...")
+        kaggle.api.dataset_download_files(dataset_path, path=".", unzip=True)
+        print("Download completed successfully.")
+    else:
+        print(f"File {file_name} already exists.")
 
 #Preprocess data
-data = pd.read_csv(file_name, delimiter=';', index_col=[0])
+'''data = pd.read_csv(file_name, delimiter=';', index_col=[0])
 data.head()
 numeric_columns = data.select_dtypes(include=['float64', 'int64']).columns
 data[numeric_columns] = data[numeric_columns].fillna(data[numeric_columns].mean())
 #data = data.fillna(data.mean(axis=0))
-print(data.head(10))
+print(data.head(10))'''
 
 
 
