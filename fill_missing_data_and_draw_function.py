e1f01fbb4600241b466d82aebcda91bc934d0162
darzen <nivnik004@gmail.com>
2024-05-08 16:15:59 +0200

    Added population_pollution_function.

--- fill_missing_data_and_draw_function.py
+++ fill_missing_data_and_draw_function.py
@@ -2,11 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from process_data import get_processed_data
+from scipy import stats
+import numpy as np
 
 
 def draw_3d_function_for_country(country: str):
     # Get preprocessed data
-    data_dict, data_df = get_processed_data()
+    _, data_df = get_processed_data()
 
     # Filtering data
     country_data = data_df[data_df['Country'] == country]
@@ -28,28 +30,32 @@ def draw_3d_function_for_country(country: str):
     ax.set_zlabel('Year')
     ax.set_title(f'{country_data['Country'].values[0]}')
 
-    data = {
-        'Year': years,
-        'Population': population,
-        'PM2.5 Pollution (µg/m³)': pollution
-    }
-
-    df = pd.DataFrame(data)
-
-    print(df)
-
     plt.show()
 
 
 def population_pollution_function(year):
-    _, population_df, pm25_df = get_preprocessed_data()
+    _, data_df = get_processed_data()
 
-    merged_df = pd.merge(pm25_df, population_df, left_on='Code', right_on='CCA3', how='inner')
+    year_data = data_df[data_df['Year'] == year]
 
+    population = year_data['Population']
+    pollution = year_data['Pollution']
 
+    z = np.polyfit(population, pollution, 1)
+    trendline = np.poly1d(z)
 
+    plt.scatter(population, pollution, marker='.')
+    plt.plot(population, trendline(population), color='red')
+
+    plt.xlabel('Population')
+    plt.ylabel('Pollution')
+    plt.title('Population vs Pollution')
+
+    plt.grid(True)
+    plt.show()
 
 
 if __name__ == "__main__":
-    draw_3d_function_for_country('Algeria')
+    #draw_3d_function_for_country('Algeria')
     #get_processed_data()
+    population_pollution_function(1990)
