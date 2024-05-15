dc17ed14bd2a7fe028fcee8a42bf92d9d37a8f9f
darzen <nivnik004@gmail.com>
2024-05-15 16:52:49 +0200

    Added linear regression model, mean squared error is too high - need additional data.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
+
+from process_data import get_processed_data
 from upload_and_preprocess_data import get_preprocessed_data
 
 import draw_functions as fmddf
 
 if __name__ == "__main__":
-    print(get_preprocessed_data()[0])
+    print(get_processed_data()[1])
     print('Enter country name: ')
     country = sys.stdin.readline().strip()
 
