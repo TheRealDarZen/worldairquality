87376e2275ba6243efd09452825182294c8a977a
darzen <nivnik004@gmail.com>
2024-05-16 18:57:13 +0200

    Added Random Forest.

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from upload_and_preprocess_data import get_preprocessed_data
 import draw_functions as fmddf
 
 if __name__ == "__main__":
-    print(get_processed_data()[1])
+    data_df = get_processed_data()[1]
+    print(data_df)
     print('Enter country name: ')
     country = sys.stdin.readline().strip()
 
-    fmddf.draw_population_pollution_year_function(country)
+    fmddf.draw_population_pollution_year_function(country, data_df)
--- random_forest_model.py
+++ random_forest_model.py
@@ -1,13 +1,12 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import matplotlib.pyplot as plt
 from process_data import get_processed_data
 
 
-def model(df):
+def random_forest_model(df):
 
     # Selecting features and target variable
     X = df[['Population', 'CO2', 'GDP', 'Vehicles', 'VPC', 'Area']]
@@ -16,25 +15,18 @@ def model(df):
     # Splitting the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
-   # Creating and training the linear regression model
-    model = LinearRegression(n_jobs=-1)
+    # Creating and training the Random Forest regression model
+    model = RandomForestRegressor(n_estimators=1000, n_jobs=-1, random_state=42)
     model.fit(X_train, y_train)
 
     # Making predictions
     y_pred = model.predict(X_test)
 
-    '''for pred, test in zip(y_pred, y_test):
-        print(f'Prediction: {pred}, Actual: {test}')'''
-
     # Evaluating the model
     mae = mean_absolute_error(y_test, y_pred)
-    print("Linear Regression - Mean Absolute Error:", mae)
+    print("Random Forest - Mean Absolute Error:", mae)
     mse = mean_squared_error(y_test, y_pred)
-    print("Linear Regression - Mean Squared Error:", mse)
-
-    # Coefficients and intercept
-    print("Coefficients:", model.coef_)
-    print("Intercept:", model.intercept_)
+    print("Random Forest - Mean Squared Error:", mse)
 
     # Calculating residuals
     residuals = y_test - y_pred
@@ -44,11 +36,11 @@ def model(df):
     plt.scatter(y_pred, residuals, alpha=0.5)
     plt.xlabel('Predicted Pollution')
     plt.ylabel('Residuals')
-    plt.title('Linear Regression Residual Plot')
+    plt.title('Random Forest Residual Plot')
     plt.axhline(y=0, color='r', linestyle='--')  # Adding a horizontal line at y=0
-    plt.savefig('LinearRegressionResidualPlot.png')
+    plt.savefig('RandomForestResidualPlot.png')
     plt.show()
 
 
 if __name__ == "__main__":
-    model(get_processed_data()[1])
+    random_forest_model(get_processed_data()[1])
